be19450461678b699fef5f3b32af06909dfc3e07
GP <gregpetropoulos@yahoo.com>
2022-08-23 13:11:43 -0400

    Footer with pagination

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
 import Contact from './components/Contact';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Footer = () => {
+  return (
+    <div className='flex justify-center m-3 w-full'>
+      <div className='btn-group'>
+        <Link to='/'>
+          <button className='btn btn-xs m-2'>1</button>
+        </Link>
+        <Link to='/contact'>
+          <button className='btn btn-xs m-2 btn-active'>2</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
