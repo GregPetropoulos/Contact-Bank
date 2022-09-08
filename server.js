ae3a22726923a65e61286373eb50074c5fc21e6d
GP <gregpetropoulos@yahoo.com>
2022-09-08 04:52:17 -0400

    Added a static server command for production environment.

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ app.use(express.json({ extended: false }));
 // *CONTACT ROUTE
 app.use('/api/contacts', require('./routes/contact'));
 
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started port ${PORT}`));
